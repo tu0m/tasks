44c92dc6b505e04186759eb7f8ede951affda30f
Kristian Tuomela <123019726+tu0m@users.noreply.github.com>
2024-04-20 16:52:03 +0300

    Initial work on createTask (WIP)

--- src/CreateTask.jsx
+++ src/CreateTask.jsx
@@ -1,13 +1,53 @@
-function CreateTask() {
+import { useEffect, useState } from "react"
+
+const initialFormData = {
+    title: "",
+    body: "",
+    project: "",
+    timestamp: "",
+    uuid: crypto.randomUUID()
+}
+
+function CreateTask({ formData = null }) {
+    const [{ title, body, project, timestamp, uuid }, setFormData] = useState(formData ?? initialFormData)
+    const [date, setDate] = useState(Date.now())
+
+    useEffect(() => {
+        let time = setInterval(() => {
+            setDate(new Date())
+        }, 1000);
+        return () => {
+            clearInterval(time);
+        };
+    })
+
+    function handleChange(event) {
+        const { name, value } = event.target
+        setFormData((prevFormData) => ({ ...prevFormData, [name]: value }))
+    }
+
+    function clearForm() {
+        setFormData({ ...initialFormData })
+        // also remove task list highlight?
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        // storage.save().then(clearForm)
+        console.log(title, body, project, timestamp, uuid)
+        // if uuid is empty, set one
+        // save to localStorage with UUID as the key
+    }
+
     return (
-        <form className="content" id="create-task">
-            <input placeholder="Title"></input>
-            <textarea placeholder="Additional Information…"></textarea>
-            <input placeholder="Project"></input>
+        <form onSubmit={handleSubmit} className="content" id="create-task">
+            <input name="title" placeholder="Title" value={title} onChange={handleChange}></input>
+            <textarea name="body" placeholder="Additional Information…" value={body} onChange={handleChange}></textarea>
+            <input name="project" placeholder="Project" value={project} onChange={handleChange}></input>
             <div>
-                <span>Apr 18</span>
-                <button>Save</button>
-                <button>Cancel</button>
+                <span>{new Intl.DateTimeFormat(undefined, { year: '2-digit', month: '2-digit', day: '2-digit' }).format(date)}</span>
+                <button type="submit">Save</button>
+                <button onClick={clearForm}>Clear</button>
             </div>
         </form>
     )
